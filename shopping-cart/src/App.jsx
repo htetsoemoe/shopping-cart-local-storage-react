9ad735783cc86241a6a71cbe051c59dbbd947756
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-08-06 13:49:43 +0630

    finished redirect to notFound page

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home'
 import Cart from './components/Cart'
 import Navbar from './components/Navbar'
+import NotFound from './components/NotFound'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/not-found-page' element={<NotFound />} />
+        <Route path='*' element={<Navigate to="/not-found-page" />} />
       </Routes>
     </div>
   )
