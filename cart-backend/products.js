84cedc723282602f49f7c201ebdd46f1742ec4c0
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-08-10 16:55:56 +0630

    styles for product cards in home

--- cart-backend/products.js
+++ cart-backend/products.js
@@ -1,15 +1,61 @@
 const products = [
+    
     {
         id: 1,
+        name: "Macbook Pro M1",
+        brand: "Apple",
+        desc: "13.5-inch display",
+        price: 2500,
+        image:
+            "https://images.pexels.com/photos/434346/pexels-photo-434346.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id: 2,
+        name: "Macbook Pro M2",
+        brand: "Apple",
+        desc: "13.5-inch display",
+        price: 3000,
+        image:
+            "https://images.pexels.com/photos/459653/pexels-photo-459653.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id: 3,
+        name: "Dell Xps 13",
+        brand: "Dell",
+        desc: "15.5-inch display",
+        price: 2000,
+        image:
+            "https://images.pexels.com/photos/811587/pexels-photo-811587.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id: 4,
+        name: "Dell Xps",
+        brand: "Dell",
+        desc: "15.5-inch display",
+        price: 1800,
+        image:
+            "https://images.pexels.com/photos/8774466/pexels-photo-8774466.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+        id: 5,
+        name: "Lenovo Thinkpad",
+        brand: "Lenovo",
+        desc: "15.5-inch display",
+        price: 2300,
+        image:
+            "https://www.pcworld.com/wp-content/uploads/2023/04/primary-1-100888614-orig.jpg?resize=1536%2C1140&quality=50&strip=all",
+    },
+    {
+        id: 6,
         name: "iPhone 13 Pro",
         brand: "Apple",
         desc: "6.1-inch display",
         price: 999,
         image:
-            "https://images.pexels.com/photos/14666017/pexels-photo-14666017.jpeg?auto=compress&cs=tinysrgb&w=600",
+            "https://images.pexels.com/photos/14666034/pexels-photo-14666034.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
-        id: 2,
+        id: 7,
         name: "iPhone 12",
         brand: "Apple",
         desc: "5.4-inch mini display",
@@ -18,7 +64,7 @@ const products = [
             "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
-        id: 3,
+        id: 8,
         name: "Galaxy S",
         brand: "Samsung",
         desc: "6.5-inch display",
--- cart-frontend/src/components/Home.jsx
+++ cart-frontend/src/components/Home.jsx
@@ -2,12 +2,37 @@ import React from 'react'
 import { useGetAllProductsQuery } from '../redux/api/productsApi'
 
 const Home = () => {
-  const {data} = useGetAllProductsQuery()
-  console.log(data);
-  
+  const { data, isLoading, error } = useGetAllProductsQuery()
+  //console.log(data);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home-container'>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occur...</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {
+              data?.map(product => {
+                return (
+                  <div key={product.id} className="product">
+                    <h3>{product.name}</h3>
+                    <img src={product.image} alt={product.name} />
+                    <div className="details">
+                      <span className='description'>{product.desc}</span>
+                      <span className="price">${product.price}</span>
+                    </div>
+                    <button>Add To Cart</button>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </>
+      )}
     </div>
   )
 }
