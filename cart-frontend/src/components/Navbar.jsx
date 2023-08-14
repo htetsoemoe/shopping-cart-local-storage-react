f2d1db7b3e5f6f7da9e538662627ef4127a9d0b5
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-08-14 19:52:58 +0630

    updated navbar font family and currency format in product card

--- cart-frontend/src/components/Navbar.jsx
+++ cart-frontend/src/components/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = () => {
     <div>
       <nav className="bg-slate-900 mr-3 h-16 mx-auto p-5 flex justify-between items-center border-b-4 w-full fixed top-0 z-30">
         <Link to={"/"}>
-          <h2 className='font-semibold text-white text-3xl ml-7 hover:text-cyan-200'>Cafe Moto</h2>
+          <h2 className='font-nunito font-semibold text-white text-3xl ml-7 hover:text-cyan-300'>Café Mötö</h2>
         </Link>
 
         <div className="flex justify-center items-center gap-5">
           <Link to={"/admin"}>
-            <p className="text-white text-lg hover:text-cyan-200">Admin</p>
+            <p className="text-white text-lg font-nunito hover:text-cyan-300 transform duration-500 hover:-translate-y-1">Admin</p>
           </Link>
 
           <Link to={"/cart"}>
-            <div className="flex text-white relative pr-7 hover:text-cyan-200">
+            <div className="flex text-white relative pr-7 hover:text-cyan-300 transform duration-500 hover:-translate-y-1">
               <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16">
                 <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
               </svg>
--- cart-frontend/src/components/Product.jsx
+++ cart-frontend/src/components/Product.jsx
@@ -3,17 +3,23 @@ import React from 'react'
 const Product = ({ product }) => {
     const { name, price, image, description } = product
 
+    // Format the price to USD using the locale, style, and currency.
+    let USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    })
+
     return (
         <div className="flex flex-col w-96 p-7 mt-3 shadow bg-slate-800 rounded transform duration-500 hover:-translate-y-2">
-            <h3 className='text-2xl text-cyan-100 font-semibold mb-5'>{name}</h3>
+            <h3 className='text-2xl text-cyan-100 font-nunito font-semibold mb-5'>{name}</h3>
             <img className='h-[200px] rounded mb-2' src={image} alt={name} />
             <div className="mt-3 mb-3">
-                <p className='text-sm text-cyan-100'>{description}</p>
+                <p className='font-nunito text-sm text-cyan-100'>{description}</p>
             </div>
             <div className="mb-5">
-                <span className="text-xl font-semibold text-yellow-300">$ {price}</span>
+                <span className="font-nunito text-xl font-semibold text-yellow-300">{USDollar.format(price)}</span>
             </div>
-            <button className='bg-green-700 hover:bg-green-500 py-2 text-black font-semibold rounded'>Add To Cart</button>
+            <button className='bg-green-700 hover:bg-green-500 py-2 text-black font-nunito font-semibold tracking-wider rounded'>Add To Cart</button>
         </div>
     )
 }
