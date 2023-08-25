156e42eac15fd425fb6d37f6df2504f413d07349
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-08-25 13:29:51 +0630

    finished cart UI

--- cart-frontend/src/components/Navbar.jsx
+++ cart-frontend/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const cartItems = useSelector(state => state.cartSlice.cartItems)
+  //console.log(cartItems);
+
   return (
     <div>
       <nav className="bg-slate-900 mr-3 h-16 mx-auto p-5 flex justify-between items-center border-b-4 w-full fixed top-0 z-30">
@@ -19,14 +23,20 @@ const Navbar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16">
                 <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
               </svg>
-              <span className="absolute bottom-4 left-7 bg-red-500 text-white w-5 text-center rounded-full">
-                <span className='item'>7</span>
-              </span>
-            </div>
-          </Link>
+              {
+                cartItems.length === 0 ? (
+                  ''
+                ): (
+                  <span className = "absolute bottom-4 left-7 bg-red-500 text-white w-5 text-center rounded-full">
+                      <span className='item'>{cartItems.length}</span>
+                  </span>
+                )
+              }
         </div>
-      </nav>
+      </Link>
     </div>
+      </nav >
+    </div >
   )
 }
 
